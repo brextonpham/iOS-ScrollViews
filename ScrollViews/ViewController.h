1edb68bd1a6abce1e249d9dd29939b62e5b24f90
Brexton Pham <brextonpham@stanford.edu>
2015-06-30 15:56:21 -0400

    Uploading Scroll Views
    Have set up initial scroll view and picture

--- ScrollViews/ViewController.h
+++ ScrollViews/ViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIScrollViewDelegate>
 
+@property (nonatomic, strong) IBOutlet UIScrollView *scrollView;
 
 @end
 
--- ScrollViews/ViewController.m
+++ ScrollViews/ViewController.m
@@ -10,18 +10,59 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIImageView *imageView;
+
+- (void)centerScrollViewContents;
+- (void)scrollViewDoubleTapped:(UITapGestureRecognizer *)recognizer;
+- (void)scrollViewTwoFingerTapped:(UITapGestureRecognizer *)recognizer;
+
 @end
 
 @implementation ViewController
 
+@synthesize scrollView = _scrollView;
+@synthesize imageView = _imageView;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // create an image view with photo1.png and set image view frame so it's the size of image and sits at 0,0
+    UIImage *image = [UIImage imageNamed:@"photo1.png"];
+    self.imageView = [[UIImageView alloc] initWithImage:image];
+    self.imageView.frame = (CGRect){.origin=CGPointMake(0.0f, 0.0f), .size=image.size};
+    [self.scrollView addSubview:self.imageView];
+    
+    // tell scroll view the size of content contained within it, so it knows how far it can scroll horizontally/vertically
+    self.scrollView.contentSize = image.size;
+    
+    // set up two gesture recognizers: double tap to zoom in and two finger tap to zoom out
+    UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scrollViewDoubleTapped:)];
+    doubleTapRecognizer.numberOfTapsRequired = 2;
+    doubleTapRecognizer.numberOfTouchesRequired = 1;
+    [self.scrollView addGestureRecognizer:doubleTapRecognizer];
+    
+    UITapGestureRecognizer *twoFingerTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(scrollViewTwoFingerTapped:)];
+    twoFingerTapRecognizer.numberOfTapsRequired = 1;
+    twoFingerTapRecognizer.numberOfTouchesRequired = 2;
+    [self.scrollView addGestureRecognizer:twoFingerTapRecognizer];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    // work out minimum zoom scale for scroll view, gives zoom scale where you can see entire image when zoomed out
+    CGRect scrollViewFrame = self.scrollView.frame;
+    CGFloat scaleWidth = scrollViewFrame.size.width / self.scrollView.contentSize.width;
+    CGFloat scaleHeight = scrollViewFrame.size.height / self.scrollView.contentSize.height;
+    CGFloat minScale = MIN(scaleWidth, scaleHeight);
+    self.scrollView.minimumZoomScale = minScale;
+    
+    // set max zoom scale as 1 to avoid blurriness, set initial zoom scale to minimum so image starts fully zoomed out
+    self.scrollView.maximumZoomScale = 1.0f;
+    self.scrollView.zoomScale = minScale;
+    
+    // center image within scroll view
+    [self centerScrollViewContents];
 }
 
 @end
